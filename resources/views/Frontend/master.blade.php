1fb99b568e264aba40cbce22f49a3ea64bf51000
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2023-11-01 22:45:41 +0100

    update css

--- resources/views/Frontend/master.blade.php
+++ resources/views/Frontend/master.blade.php
@@ -163,8 +163,8 @@
                             <img class="img-fluid" src="img/carousel-1.jpg" alt="Image">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
-                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">Reach your Full Potentiel</h4>
-                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">with our Suppliments</h3>
+                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
+                                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Fashionable Dress</h3>
                                     <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
@@ -173,7 +173,7 @@
                             <img class="img-fluid" src="img/carousel-2.jpg" alt="Image">
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style="max-width: 700px;">
-                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">BECOME THE 1%</h4>
+                                    <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First Order</h4>
                                     <h3 class="display-4 text-white font-weight-semi-bold mb-4">Reasonable Price</h3>
                                     <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
@@ -190,7 +190,7 @@
                             <span class="carousel-control-next-icon mb-n2"></span>
                         </div>
                     </a>
-                </div>
+                </div> 
             </div>
         </div>
     </div>
